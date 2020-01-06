7d1accb2306113429d0abe493acb5574838dbd7c
Piotr Król <piotr.krol@3mdeb.com>
2020-01-06 22:58:53 +0100

    oops: add more debug logs to simplify dyndebug exercise
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- oops.c
+++ oops.c
@@ -8,22 +8,24 @@ MODULE_LICENSE("GPL");
 
 static noinline void do_oops(void)
 {
-    *(int*)0x42 = 'a';
+    *(int*)0x42 = 'o';
 }
 
 static int kh_oops_init(void)
 {
-    pr_debug("dbg: kh_oops_init\n");
+    pr_debug("dbg: kh_oops_init before do_oops\n");
     pr_info("kh_oops_init\n");
     //do_oops();
+    pr_debug("dbg: kh_oops_init after do_oops\n");
 
     return 0;
 }
 
 static void kh_oops_exit(void)
 {
-    pr_debug("dbg: kh_oops_exit\n");
+    pr_debug("dbg: kh_oops_exit 1\n");
     pr_info("kh_oops_exit\n");
+    pr_debug("dbg: kh_oops_exit 2\n");
 }
 
 module_init(kh_oops_init);
